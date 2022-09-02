eb0a21c0f3b3a38f4e3732719f75c8d20b47af2a
n.akhmetov <nurakmedy@gmail.com>
2022-09-02 16:53:15 +0600

    init

---
+++ src/routes/auth.js
@@ -0,0 +1,83 @@
+const {Router} = require('express');
+const db = require("../db/db");
+const {hashPassword, comparePassword} = require('../utils/helpers');
+
+const router = Router();
+
+//Вход пользователя
+router.post('/login', async (request, response) => {
+    const {username, password} = request.body;
+
+    if (!username || !password) {
+        return response.send(400);
+    }
+
+    const userDB = await db('users')
+        .where('username', username)
+        .select('username', 'password')
+        .first();
+
+    if (!userDB) {
+        return response.send(401);
+    }
+
+    const isPasswordValid = comparePassword(password, userDB.password);
+
+    if (isPasswordValid) {
+        console.log('Authenticated successfully!');
+        //сохраняем пользователя
+        request.session.user = userDB;
+
+        return response.send(200);
+    }
+    else {
+        console.log('Failed to authenticate');
+
+        return response.send(401);
+    }
+});
+
+//Регистрация нового пользователя
+router.post('/register', async (req, res) => {
+    const {username, password, email} = req.body;
+
+    const userDB = await db('users')
+        .where('username', username)
+        .andWhere('email', email)
+        .select('username', 'email')
+        .first();
+
+    if (userDB) {
+        res.status(400).send({msg: 'User already exists!'});
+    } else {
+        const hashedPassword = hashPassword(password);
+
+        await db('users')
+            .insert({username, email, password: hashedPassword});
+
+        res.status(201).send({msg: 'New user has been added!'});
+    }
+});
+
+//Проверка сессии текущего пользователя
+router.use((req, res, next) => {
+    if (req.session.user) {
+        next();
+    } else {
+        res.send(401);
+    }
+});
+
+//Список пользователей. Нельзя получить список если пользователь не выполнил вход
+router.get('/users', async (req, res) => {
+    let users = await db('users')
+        .select('id', 'username', 'email');
+
+    if (users) {
+        res.status(200).send(users);
+    } else {
+        res.status(404).send('No users found');
+    }
+});
+
+module.exports = router;
